5eda4d402e69e8490fa2ed1ccdf180829100e150
saldocc <onurcoban1109@gmail.com>
2021-05-14 18:09:01 +0300

    Added commit form and Auth0 integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import '../styles/globals.css'
 import Header from "../components/header"
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-indigo-50 min-h-screen antialiased text-gray-800">
-      <Header />
-      <main className="mt-6 mb-20">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="saldoc.eu.auth0.com"
+      clientId={process.env.NEXT_PUBLIC_AUTHO_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="bg-indigo-50 min-h-screen antialiased text-gray-800">
+        <Header />
+        <main className="mt-6 mb-20">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   )
 }
 
